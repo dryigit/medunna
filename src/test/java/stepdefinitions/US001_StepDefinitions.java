848b7ab25efef216a18e1c929ac67b7ab7a6bdc4
Mehmet Yigit <mfyigit@MyMacBookAir.local>
2022-03-30 22:45:35 +0300

    JDK11-working

--- src/test/java/stepdefinitions/US001_StepDefinitions.java
+++ src/test/java/stepdefinitions/US001_StepDefinitions.java
@@ -1,4 +1,14 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class US001_StepDefinitions {
-}
+    @Given("mf kullanici medunna sayfasina gider")
+    public void kullanici_medunna_sayfasina_gider() {
+        Driver.getDriver().get(ConfigReader.getProperty("MedunnaURL"));
+
+    }
+    }
+
