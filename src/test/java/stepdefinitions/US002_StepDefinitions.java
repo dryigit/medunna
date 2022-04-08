c63047e5719a8847d1e903e2823a961e5f08d0f7
Mehmet Yigit <mfyigit@MyMacBookAir.local>
2022-04-08 16:32:50 +0300

    us001 and us002

--- src/test/java/stepdefinitions/US002_StepDefinitions.java
+++ src/test/java/stepdefinitions/US002_StepDefinitions.java
@@ -14,45 +14,34 @@ public class US002_StepDefinitions {
      String password = faker.internet().password();
     US002Page us002page=new US002Page();
 
-    @Then("kullanici username girer")
-    public void kullaniciUsernameGirer() {
-        String username = faker.name().username();
-        actions.click(us002page.usernamrBox)
-                .sendKeys(username)
-                .sendKeys(Keys.DOWN)
-                .sendKeys(Keys.DOWN)
-                .perform();
+
+   @Then("kullanici {string} girer")
+   public void kullaniciGirer(String username) {
+       actions.click(us002page.usernamrBox).sendKeys(username).perform();
+   }
+    @Then("kullanici {string} yazar ve sayfayi asagi kaydirir")
+    public void kullaniciYazar(String email) {
+       actions.click(us002page.emailBox).sendKeys(email).
+               sendKeys(Keys.TAB).perform();
+    }
+
+
+    @Then("kullanici {string} girilebilmelidir")
+    public void kullaniciGirilebilmelidir(String ilkPassword) {
+       actions.click(us002page.emailBox).sendKeys(Keys.TAB).sendKeys(ilkPassword).sendKeys(Keys.TAB).perform();
 
     }
 
-    @Then("kullanici email girer")
-    public void kullaniciEmailGirer() {
-        String email = faker.internet().emailAddress();
-        actions.click(us002page.emailBox)
-                .sendKeys(email)
-                .perform();
-
-    }
-
-    @Then("kullanici password girilebilmelidir")
-    public void kullaniciPasswordGirilebilmelidir() {
-
-        actions.click(us002page.passwordBox)
-                .sendKeys(password)
-                .sendKeys(Keys.ARROW_DOWN).perform();
-
-
-    }
-
-    @Then("kullanici password ikinci defa girilebilmelidir")
-    public void kullaniciPasswordIkinciDefaGirilebilmelidir() {
-        actions.click(us002page.confirmPasswordBox)
-                .sendKeys(password);
-
+    @Then("kullanici {string} ikinci defa girilebilmelidir")
+    public void kullaniciIkinciDefaGirilebilmelidir(String ikinciPassword) {
+       actions.sendKeys(Keys.TAB).click(us002page.confirmPasswordBox).sendKeys(ikinciPassword).perform();
     }
 
     @Then("kullanici register butonuna tıklar")
     public void kullaniciRegisterButonunaTıklar() {
         actions.click(us002page.registerButton2).perform();
     }
+
+
+
 }
--- src/test/resources/features/US002.feature
+++ src/test/resources/features/US002.feature
@@ -1,25 +1,32 @@
 Feature: US002_Email ve Username
-  Scenario: TC01_Kayıt olmak için girilebilmelidir
+  Scenario Outline: TC01_Kayıt olmak için girilebilmelidir
     Given mf kullanici "MedunnaURL" sayfasina gider
     And mf kullanici menüde bulunan account simgesine tıklar
     And mf kullanici register butonuna tıklar
-    Then kullanici username girer
+    Then kullanici "<username>" girer
+    Examples: test data
+      |username|
+      |alicankara|
 
-  Scenario: TC02_Kayıt olmak için girilebilmelidir
+  Scenario Outline:TC02_Kayıt olmak için girilebilmelidir
     Given mf kullanici "MedunnaURL" sayfasina gider
     And mf kullanici menüde bulunan account simgesine tıklar
     And mf kullanici register butonuna tıklar
-    Then kullanici email girer
+    Then kullanici "<email>" yazar ve sayfayi asagi kaydirir
+    Examples: test data
+      |email|
+      |alicankara@medunna.com|
 
-  Scenario: TC03_Kayıt olmak için girilebilmelidir
+  Scenario Outline:: TC03_Kayıt olmak için girilebilmelidir
     Given mf kullanici "MedunnaURL" sayfasina gider
     And mf kullanici menüde bulunan account simgesine tıklar
     And mf kullanici register butonuna tıklar
-    Then kullanici email girer
-    Then kullanici password girilebilmelidir
-    Then kullanici password ikinci defa girilebilmelidir
-    Then kullanici register butonuna tıklar
-    And
+    Then kullanici "<ilkPassword>" girilebilmelidir
+    Then kullanici "<ikinciPassword>" ikinci defa girilebilmelidir
+  Then kullanici register butonuna tıklar
+    Examples: test data
+      |ilkPassword|ikinciPassword|
+      |Alicankara20!|Alicankara20!|
 
 
 
