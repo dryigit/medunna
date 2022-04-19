e5ac54ee60c12ed75b945c549e23f48eddb9433f
Abdulbakı Gumus <agummus038@gmail.com>
2022-04-19 22:10:48 +0300

    adbulbaki3

--- src/test/java/stepdefinitions/US25_StepDefinitions.java
+++ src/test/java/stepdefinitions/US25_StepDefinitions.java
@@ -5,16 +5,19 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import pages.US25_Page;
 import pages.US9Page;
 import utilities.ConfigReader;
 import utilities.Driver;
-
+import utilities.JSUtils;
+import utilities.ReusableMethods;
 
 
 public class US25_StepDefinitions {
-US25_Page us25_page=new US25_Page();
-    US9Page us9Page =new US9Page();
+    US25_Page us25_page = new US25_Page();
+    US9Page us9Page = new US9Page();
 
     @Given("PatientAG {string} sayfasina gider")
     public void patientagSayfasinaGider(String a) {
@@ -38,12 +41,12 @@ US25_Page us25_page=new US25_Page();
     public void patientagMyPagesSayfasiAltindaMakeAnAppointmentButonunaTiklar() {
         us25_page.myPagesbuttonPatient.click();
         us25_page.makeAnAppointment.click();
-        
+
     }
 
     @Then("PatientAG firstname,lastname,SSN,email,phone,appointment date kutularini doldurur")
     public void patientagFirstnameLastnameSSNEmailPhoneAppointmentDateKutulariniDoldurur() {
-       us25_page.firstName.clear();
+        us25_page.firstName.clear();
         us25_page.firstName.sendKeys("Ali");
         us25_page.lastName.clear();
         us25_page.lastName.sendKeys("Baba");
@@ -54,37 +57,43 @@ US25_Page us25_page=new US25_Page();
         us25_page.phone.clear();
         us25_page.phone.sendKeys("123-345-1234");
 
-        
+
     }
 
     @And("PatientAG Send an appointment request butonuna tiklayarak cikan mesajdan randevu olusturuldugunu dogrular")
     public void patientagSendAnAppointmentRequestButonunaTiklayarakCikanMesajdanRandevuOlusturuldugunuDogrular() {
-    //    us25_page.sendAppnmtButton.click();
-    //    Assert.assertTrue(us25_page.toastify.isDisplayed());
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) Driver.getDriver();
+
+        javascriptExecutor.executeScript("window.scrollBy(0,500)");
+
+        JSUtils.clickElementByJS(us25_page.sendAppnmtButton);
+        ReusableMethods.waitForVisibility(us25_page.toasttify, 5);
+        Assert.assertTrue(us25_page.toasttify.isDisplayed());
     }
 
     @And("PatientAG Contact sayfasina tiklar")
     public void patientagContactSayfasinaTiklar() {
-        us25_page.contactButton.click();
-        
+
+        JSUtils.clickElementByJS(us25_page.contactButton);
     }
 
     @Then("PatientAG name,email,subject,message kutularini doldurur")
     public void patientagNameEmailSubjectMessageKutulariniDoldurur() {
         JavascriptExecutor javascriptExecutor = (JavascriptExecutor) Driver.getDriver();
 
-        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);",us25_page.contactName);
+        javascriptExecutor.executeScript("arguments[0].scrollIntoView(true);", us25_page.contactName);
 
         us25_page.contactName.sendKeys("Cabbar");
         us25_page.contactEmail.sendKeys("test@gmail.com");
         us25_page.contactSubject.sendKeys("bravo");
         us25_page.contactMessage.sendKeys("tebrıkler");
 
-        us25_page.contactSendButton.click();
+     JSUtils.clickElementByJS( us25_page.contactSendButton);
     }
 
     @And("PatientAG Send butonuna tiklayarak cikan mesajdan iletisim istegi gönderildigini dogrular")
     public void patientagSendButonunaTiklayarakCikanMesajdanIletisimIstegiGönderildiginiDogrular() {
+        Assert.assertTrue(us25_page.toastMessage.isDisplayed());
     }
 
 
