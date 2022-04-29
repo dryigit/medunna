7914c573c88109c5bed4d68d459729feb214283c
mehmet surucu <mehmetsmarket@gmail.com>
2022-04-29 11:21:39 +0300

    29 Apr

--- src/test/java/stepdefinitions/US008_StepDefinitions.java
+++ src/test/java/stepdefinitions/US008_StepDefinitions.java
@@ -4,10 +4,10 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
-import org.openqa.selenium.support.ui.Select;
 import pages.US008Page;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class US008_StepDefinitions {
 
@@ -28,7 +28,7 @@ public class US008_StepDefinitions {
     }
 
     @Then("adminOkan kullanici adini {string} girer")
-    public void adminOkankullanıciAdiniGirer(String username) {
+    public void adminOkankullaniciAdiniGirer(String username) {
         us008Page.username.sendKeys(username);
     }
 
@@ -43,6 +43,8 @@ public class US008_StepDefinitions {
 
         us008Page.signInSignInButton.submit();
     }
+
+    /*
 
     @And("adminOkan ikon altindaki password sekmesine tiklar")
     public void adminOkanikonAltindakiPasswordSekmesineTiklar() {
@@ -79,6 +81,8 @@ public class US008_StepDefinitions {
 
         Assert.assertTrue(us008Page.toastify.isEnabled());
     }
+    */
+
 
     @Then("staffHakan giris yapilamadigini kontrol eder")
     public void staffHakangirisYapilamadiginiKontrolEder() {
@@ -87,30 +91,65 @@ public class US008_StepDefinitions {
 
     }
 
-    @Given("staffHakan medunna anasayfasına gider")
-    public void staffhakanMedunnaAnasayfasınaGider() {
+
+    @Given("staffHakan medunna anasayfasina gider")
+    public void staffhakanMedunnaAnasayfasinaGider() {
         Driver.getDriver().get(ConfigReader.getProperty("MedunnaURL"));
-
     }
 
-    @Given("kullanıcı yeni sifresini girer {string}")
-    public void kullanıcıYeniSifresiniGirer(String newPassword) {
+    @And("staffHakan giris yapmak icin ikon altindaki signIn'e tiklar")
+    public void staffhakanGirisYapmakIcinIkonAltindakiSignInETiklar() {
+        us008Page.ikon.click();
+        us008Page.ikonSignIn.click();
+    }
 
+    @Then("staffHakan kullanici adini {string} girer")
+    public void staffhakanKullaniciAdiniGirer(String username) {
+        us008Page.username.sendKeys(username);
+    }
+
+    @And("staffHakan sifresini {string} girer")
+    public void staffhakanSifresiniGirer(String password) {
+        us008Page.password.sendKeys(password);
+    }
+
+    @And("staffHakan signIn sayfasindaki signIn butonuna tiklar")
+    public void staffhakanSignInSayfasindakiSignInButonunaTiklar() {
+        us008Page.signInSignInButton.submit();
+    }
+
+    @And("staffHakan ikon altindaki password sekmesine tiklar")
+    public void staffhakanIkonAltindakiPasswordSekmesineTiklar() {
+        us008Page.ikinciIkon.click();
+        us008Page.passwordButton.click();
+    }
+
+    @And("staffHakan gecerli sifresini {string} girer")
+    public void staffhakanGecerliSifresiniGirer(String currentPassword) {
+        us008Page.currentPassword.sendKeys(currentPassword);
+    }
+
+    @And("staffHakan yeni sifresini girer {string}")
+    public void staffhakanYeniSifresiniGirer(String newPassword) {
         us008Page.newPassword.sendKeys(newPassword);
     }
 
-
-    @Given("kullanıcı en az yedi karakterli yeni sifre{string} girer")
-    public void kullanıcıEnAzYediKarakterliYeniSifreGirer(String newPassword) {
-        us008Page.newPassword.sendKeys(newPassword);
-    }
-
-    @Then("sifre zorluk seviyesinin {string} oldugunu dogrular")
-    public void sifreZorlukSeviyesininOldugunuDogrular(String level) throws InterruptedException {
-
+    @Then("staffHakan sifre zorluk seviyesinin {string} oldugunu dogrular")
+    public void staffhakanSifreZorlukSeviyesininOldugunuDogrular(String level) throws InterruptedException {
         sifreZorlukSeviyesi(level);
-
     }
+
+    @Given("staffHakan yeni sifre kutusunu temizler")
+    public void staffhakanYeniSifreKutusunuTemizler() {
+        ReusableMethods.waitForClickablility(us008Page.newPassword, 3);
+        us008Page.newPassword.clear();
+    }
+
+    @And("staffHakan en az yedi karakterli yeni sifre{string} girer")
+    public void staffhakanEnAzYediKarakterliYeniSifreGirer(String newPassword) {
+        us008Page.newPassword.sendKeys(newPassword);
+    }
+
 
     public void sifreZorlukSeviyesi(String level) throws InterruptedException {
             // switch case ile yapılabilir
@@ -141,10 +180,7 @@ public class US008_StepDefinitions {
         }
     }
 
-    @Given("kullanıcı yeni şifre kutusunu temizler")
-    public void kullanıcıYeniŞifreKutusunuTemizler() {
 
-        us008Page.newPassword.clear();
-    }
+
 }
 
